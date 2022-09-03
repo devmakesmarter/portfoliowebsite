b0a61591da3a276f8d84d33a227b9b55acba8ab1
Easy <erdogan.isma@gmail.com>
2022-09-03 08:49:47 +0200

    03.09.2022 Pop up dokumentiert

--- src/Components/Popup/Popup.js
+++ src/Components/Popup/Popup.js
@@ -1,35 +1,42 @@
-import "../Popup/Popup.css";
+import "../Popup/PopUp.css";
 
 
 
-export const PopUp = ({description, visible, event, img}) => {
+export const PopUp = ({ description, visible, event, img }) => {
 
- 
 
-    if(!visible) {
+    /* Dieses Pop´Up ist wie folgt aufgebaut. Die Props werden von der Hauptkomponente runtergegeben. Grundsätzlich ist das Pop´Up nicht sichtbar. 
+       Die Sichtbarkeit wird über die Variable visible gesteurt. Die Inhalte des werden auch von der Hauptkompnente runter gegeben. Auch die Funktions für das Schliessen und Öffnen. 
+
+    */
+
+    if (!visible) {
         return null
     }
 
 
+    return (
 
-return( 
-    <div> 
-
-
-    <div className="containerfromcontainerfrompopup"> 
-    <div className="containerfrompopup"> 
-    
-    <div className="divforbeschreibung" >Beschreibung </div>
-    <div className="divforimage" ><img className="imgself" alt="" src={img}></img></div>
-    <div  className="description" >{description}</div>
-    <div className="divforbutton" > <button onClick={event} className="buttonself" > Schliessen</button> </div>
-    
-    </div> 
-    </div>
-
-
-    </div>
-)
+        <
+        >
+        <
+        div className = "containerfromcontainerfrompopup" >
+        <
+        div className = "containerfrompopup" >
+        <
+        div className = "divforbeschreibung" > Beschreibung < /div> <
+        div className = "divforimage" > < img className = "imgself"
+        alt = ""
+        src = { img } > < /img></div >
+        <
+        div className = "description" > { description } < /div> <
+        div className = "divforbutton" > < button onClick = { event }
+        className = "buttonself" > Schliessen < /button> </div >
+        <
+        /div>  <
+        /div> <
+        />
+    )
 
 
 };
