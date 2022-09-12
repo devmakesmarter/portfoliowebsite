26ca7d1a62fc37c52f690c35c1798a1b53ccf39d
Easy <erdogan.isma@gmail.com>
2022-09-12 12:23:13 +0200

    bootstrapeins

--- src/Components/Firststepwithbootstrap/Firsstepwithbootstrap.js
+++ src/Components/Firststepwithbootstrap/Firsstepwithbootstrap.js
@@ -1,16 +1,54 @@
-import React from 'react';
+import {React, useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Firsstepwithbootstrap.css";
+import { Container, Row, Col, Carousel  } from 'react-bootstrap';
+import { Quizapp } from '../Quizapp/Quiz';
+import {ToDo} from "../ToDo/ToDo";
+import pictureone from "../Popup/pexels-lilartsy-1194775.jpg";
+
 
 
 export const Firsstepwithbootstrap = () => {
 
+const [index, setIndex] = useState(0);
 
-    return ( <
-        div >
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
 
-        <
-        /div>
-    )
+
+
+
+ const desgin = {height: "200px"};
+
+
+  return (
+    <>
+      <Container id="caruselspeziale" className="container" > 
+             <Row  >
+           <form >
+            <Col className="col-12 col-md-4"><input className="myinput"  type="text"  placeholder="Bitte tippen Sie hier" maxLength="50" required ></input></Col>
+            <Col className="col-12 col-md-4"><button className="" type="submit" >E</button></Col>
+            <Col className="col-12 col-md-4"><input className="myinput"  type="text"  placeholder="Bitte tippen Sie hier" maxLength="50" required ></input></Col>
+            </form>
+            </Row >
+
+
+     
+
+        <form className="row">
+            <Col className="col-12 col-md-4"><input className="myinput"  type="text"  placeholder="Bitte tippen Sie hier" maxLength="50" required ></input></Col>
+            <Col className="col-12 col-md-4"><button className="" type="submit" >E</button></Col>
+            <Col className="col-12 col-md-4"><input className="myinput"  type="text"  placeholder="Bitte tippen Sie hier" maxLength="50" required ></input></Col>
+        </form>
+        
+
+        <Row  >
+       
+        </Row>
+      </Container>
+    </>
+  )
 }
 
 export default Firsstepwithbootstrap;
